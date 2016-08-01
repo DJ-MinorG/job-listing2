f87fa5c73fcd9050f26cf8270a141bc076624d4c
Jane Du <Jane.dujuan.hr@gmail.com>
2016-08-02 06:23:09 +0800

    add hide and publish button

--- job-listing2/app/controllers/admin/jobs_controller.rb
+++ job-listing2/app/controllers/admin/jobs_controller.rb
@@ -53,13 +53,19 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def is_hidden
+    @job=Job.find(params[:id])
+    @job.is_hidden!
+    redirect_to :back
+  end
+
+
   private
 
   def params_job
-    params.require(:job).permit(:title, :description,:wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description,:wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
 
   end
-
 
 
 end
--- job-listing2/app/models/job.rb
+++ job-listing2/app/models/job.rb
@@ -3,4 +3,15 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  def is_hidden!
+    if self.is_hidden
+      self.is_hidden = false
+      self.save
+    else
+      self.is_hidden = true
+      self.save
+    end
+
+  end
 end
