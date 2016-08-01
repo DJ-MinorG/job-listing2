bb50f380edec5be68563a2d722420676ca20be49
Jane Du <Jane.dujuan.hr@gmail.com>
2016-08-01 20:40:26 +0800

    implement step2

--- job-listing2/app/controllers/jobs_controller.rb
+++ job-listing2/app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create,:edit,:update,:destroy]
+  # before_filter :authenticate_user!, only: [:new, :create,:edit,:update,:destroy]
 
 
   def index
@@ -11,47 +11,10 @@ class JobsController < ApplicationController
   end
 
 
-  def new
-    @job=Job.new
-  end
 
-  def create
 
-    @job=Job.new(params_job)
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
 
-    end
-  end
 
-  def edit
-    @job=Job.find(params[:id])
-
-  end
-
-  def update
-    @job=Job.find(params[:id])
-    if @job.update(params_job)
-      redirect_to jobs_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @job=Job.find(params[:id])
-    @job.destroy
-    redirect_to jobs_path
-  end
-
-  private
-
-  def params_job
-    params.require(:job).permit(:title, :description)
-
-  end
 
 
 end
